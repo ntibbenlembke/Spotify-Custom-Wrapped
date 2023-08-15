118b92825d85ea729db4e94017b387e12bba886d
tibben2 <tibben2@stolaf.edu>
2023-08-15 13:21:21 -0500

    First round complete- spotify artist search

---
+++ client/src/App2.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap\dist\css\bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
+
+
+function App()  {
+  return (
+    <div className="App">
+      <Container>
+        <InputGroup className='mb-3' size='large'>
+        </InputGroup>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
